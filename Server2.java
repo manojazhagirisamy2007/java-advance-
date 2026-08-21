808ec7684f838ed825d031f1ded2148cab1bb3e4
manojazhagirisamy2007 <manojazhagirisamy2007@gmail.com>
2026-08-21 11:10:47 +0530

    Add files via upload

---
+++ Server2.java
@@ -0,0 +1,50 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+class Server2 {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket ss = new ServerSocket(5000);
+
+        System.out.println("Waiting for client...");
+        Socket s = ss.accept();
+        System.out.println("Client Connected");
+
+        Scanner sc = new Scanner(System.in);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(s.getInputStream()));
+
+        PrintWriter out = new PrintWriter(
+                s.getOutputStream(), true);
+
+        while (true) {
+
+            // Server sends message
+            System.out.print("Server: ");
+            String msg = sc.nextLine();
+            out.println(msg);
+
+            if (msg.equalsIgnoreCase("bye"))
+                break;
+
+            // Receive Client message
+            String reply = in.readLine();
+
+            if (reply == null)
+                break;
+
+            System.out.println("Client: " + reply);
+
+            if (reply.equalsIgnoreCase("bye"))
+                break;
+        }
+
+        in.close();
+        out.close();
+        s.close();
+        ss.close();
+        sc.close();
+    }
+}
